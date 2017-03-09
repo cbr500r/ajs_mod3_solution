768ee8f41a21ad032859e0e53d3a0fe8fcc2a172
evansjr <evansjr@nih.gov>
2017-03-09 17:11:27 -0500

    first final

---
+++ app.js
@@ -0,0 +1,68 @@
+(function() {
+	'use strict';
+
+	angular.module('NarrowItDownApp', [])
+		.controller('NarrowItDownController', NarrowItDownController)
+		.service('MenuSearchService', MenuSearchService)
+		.directive('foundItems', foundItems)
+		;
+
+	NarrowItDownController.$inject = ['MenuSearchService'];
+
+	function NarrowItDownController(MenuSearchService){
+		var ndown = this;
+		ndown.term = '';
+		ndown.found = [];
+
+		ndown.search = function(term){
+			MenuSearchService.getMatchedMenuItems(term)
+				.then(function(found){
+				ndown.found = found;
+			});
+		}
+		
+		ndown.remove = function(index){
+			ndown.found.splice(index, 1);
+		}
+	}
+
+	MenuSearchService.$inject = ['$http'];
+	function MenuSearchService($http){
+		var service = this;
+
+		var menu_items_url = 'https://davids-restaurant.herokuapp.com/menu_items.json';
+		
+		service.getMatchedMenuItems = function(searchTerm){
+			var re = new RegExp(searchTerm, 'i');
+
+			return $http.get(menu_items_url).then(function (result) {
+			    // process result and only keep items that match
+			    var foundItems = result.data.menu_items.filter(function(item){
+			    	return re.test(item.description);
+			    });
+
+			    // return processed items
+			    return foundItems;
+			});
+		}
+	}
+
+	function foundItemDirectiveController(){
+		var list = this;
+	}
+
+	function foundItems(){
+		return {
+			restrict: 'E',
+			scope: {
+				items: '<',
+				onRemove: '&',
+			},
+			templateUrl: 'foundItems.html',
+			controller: foundItemDirectiveController,
+			controllerAs: 'list',
+			bindToController: true
+		};
+	}
+	
+})();
